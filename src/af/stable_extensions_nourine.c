5191f43b6126e067a06b70764d05435e88b92746
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2023-09-25 08:22:50 +0200

    Started implementation of the Nourine approach.

---
+++ src/af/stable_extensions_nourine.c
@@ -0,0 +1,223 @@
+/*
+ * AFCA - argumentation framework using closed sets
+ *
+ * Copyright (C) Baris Sertkaya (sertkaya@fb2.fra-uas.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+
+#include "../fca/context.h"
+#include "../bitset/bitset.h"
+
+// In our case implications are unit implications.
+// lhs is a bitset, rhs is an index
+struct unit_implication {
+	BitSet *lhs;
+	int rhs;
+};
+
+typedef struct unit_implication UnitImplication;
+
+UnitImplication *create_unit_implication(int size) {
+	UnitImplication *imp = (UnitImplication*) calloc(1, sizeof(UnitImplication));
+	assert(imp != NULL);
+	imp->lhs = create_bitset(size);
+	imp->rhs = -1;
+
+	return(imp);
+}
+
+void free_implication(UnitImplication *imp) {
+	free_bitset(imp->lhs);
+	free(imp);
+}
+
+// Implication set is just an array of implications
+struct implication_set {
+	int size;
+	UnitImplication **elements;
+};
+
+typedef struct implication_set ImplicationSet;
+
+ImplicationSet *create_implication_set() {
+	ImplicationSet *imps = (ImplicationSet*) calloc(1, sizeof(ImplicationSet));
+	assert(imps != NULL);
+	imps->size = 0;
+	imps->elements = NULL;
+
+	return(imps);
+}
+
+void add_implication(UnitImplication *imp, ImplicationSet *imps) {
+	UnitImplication** tmp;
+	tmp = realloc(imps->elements, (imps->size + 1) * sizeof(UnitImplication*));
+	assert(tmp != NULL);
+	imps->elements = tmp;
+	imps->elements[imps->size] = imp;
+	++imps->size;
+}
+
+ImplicationSet *attacks_to_implications(Context *attacks) {
+	ImplicationSet *imps = create_implication_set();
+
+	int i, j;
+	for (i = 0; i < attacks->size; ++i) {
+		for (j = 0; j < attacks->size; ++j) {
+			if (TEST_BIT(attacks->a[i], j) && !TEST_BIT(attacks->a[j], i)) {
+				// TODO
+			}
+		}
+	}
+
+	return(imps);
+}
+
+// Compute the next conflict-free closure coming after "current" and store it in "next"
+char next_closure(Context* not_attacks, Context* attacks, BitSet* current, BitSet* next) {
+	int i,j;
+	BitSet* tmp = create_bitset(current->size);
+
+	for (i = not_attacks->size - 1; i >= 0; --i) {
+		if (TEST_BIT(current, i))
+			RESET_BIT(current, i);
+		else {
+			// check if i attacks i
+			if (TEST_BIT(attacks->a[i], i))
+					continue;
+
+			// check if argument i attacks the set current
+			bitset_intersection(current, attacks->a[i], tmp);
+			if (!bitset_is_emptyset(tmp))
+				continue;
+
+			// check if current attacks i
+			char flag = 0;
+			for (j = 0; j < current->size; ++j) {
+				if (TEST_BIT(current,j) && TEST_BIT(attacks->a[j], i)) {
+					flag = 1;
+					break;
+				}
+			}
+			if (flag)
+				continue;
+
+			reset_bitset(tmp);
+			SET_BIT(current, i);
+
+			// compute next
+			down_up_arrow(not_attacks, current, next);
+			RESET_BIT(current, i);
+			// TODO: optimize!
+			bitset_set_minus(next, current, tmp);
+			flag = 0;
+			for (j = 0; j < i; ++j)
+				// check if next \ current contains a bit larger than i
+				if (TEST_BIT(tmp, j)) {
+					flag = 1;
+					break;
+				}
+			if (!flag)
+				return(1);
+		}
+	}
+	return(0);
+}
+
+void all_stable_extensions_nc(Context* attacks, FILE *outfile) {
+
+	Context* not_attacks = negate_context(attacks);
+
+	BitSet* tmp = create_bitset(attacks->size);
+	BitSet* nc = create_bitset(attacks->size);
+	BitSet* nc_up = create_bitset(attacks->size);
+
+	int closure_count = 0, stable_extension_count = 0;
+
+	while (1) {
+		if (!next_closure(not_attacks, attacks, tmp, nc))
+			break;
+		++closure_count;
+		// printf("*");
+		// print_bitset(ni, stdout);
+		// printf("\n");
+
+		up_arrow(not_attacks, nc, nc_up);
+		// ni is closed but has a conflict
+		// if (!bitset_is_subset(ni, tmp)) {
+		// 	printf("*");
+		// 	print_bitset(ni, stdout);
+		// 	printf("\n");
+		// }
+		if (bitset_is_equal(nc, nc_up)) {
+			++stable_extension_count;
+			print_bitset(nc, outfile);
+			fprintf(outfile, "\n");
+		}
+		copy_bitset(nc, tmp);
+	}
+	printf("Number of closures generated: %d\n", closure_count);
+	printf("Number of stable extensions: %d\n", stable_extension_count);
+
+	free_bitset(tmp);
+	free_bitset(nc);
+	free_bitset(nc_up);
+
+	free_context(attacks);
+	free_context(not_attacks);
+}
+
+void one_stable_extension_nc(Context* attacks, FILE *outfile) {
+
+	Context* not_attacks = negate_context(attacks);
+
+	BitSet* tmp = create_bitset(attacks->size);
+	BitSet* nc = create_bitset(attacks->size);
+	BitSet* nc_up = create_bitset(attacks->size);
+
+	int closure_count = 0;
+
+	while (1) {
+		if (!next_closure(not_attacks, attacks, tmp, nc))
+			break;
+		++closure_count;
+		// printf("*");
+		// print_bitset(ni, stdout);
+		// printf("\n");
+
+		up_arrow(not_attacks, nc, nc_up);
+		// ni is closed but has a conflict
+		// if (!bitset_is_subset(ni, tmp)) {
+		// 	printf("*");
+		// 	print_bitset(ni, stdout);
+		// 	printf("\n");
+		// }
+		if (bitset_is_equal(nc, nc_up)) {
+			print_bitset(nc, outfile);
+			fprintf(outfile, "\n");
+			break;
+		}
+		copy_bitset(nc, tmp);
+	}
+	printf("Number of closures generated: %d\n", closure_count);
+
+	free_bitset(tmp);
+	free_bitset(nc);
+	free_bitset(nc_up);
+
+	free_context(attacks);
+	free_context(not_attacks);
+}
