ff093511a30b275b54c5f7885a43ffabc17b72a5
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2023-11-13 17:27:31 +0100

    Refactoring af and bitset.

--- src/bitset/bitset.h
+++ src/bitset/bitset.h
@@ -21,29 +21,30 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include "../af/af.h"
 
 // Base type for storing the bits.
 #define BITSET_BASE_TYPE	uint64_t
-// Number of bytes in the base type.
+
+// Number of bits in the base type.
 #define BITSET_BASE_SIZE	(8*sizeof(BITSET_BASE_TYPE))
 
 typedef struct bitset BitSet;
 
 // A BitSet is an array of BITSET_BASE_TYPE
 struct bitset {
-	int base_count;
-	int size;
 	BITSET_BASE_TYPE* elements;
 };
 
 // Create an empty bitset of the given size and return the address.
-BitSet* create_bitset(int size);
+BitSet* create_bitset(AF *af);
 
 // Free the memory allocated for the bitset bs.
-void free_bitset(BitSet* bs);
+// Returns the number of bytes freed
+int free_bitset(AF *af, BitSet* bs);
 
 // Print the given bitset
-void print_bitset(BitSet* bs, FILE *f);
+void print_bitset(AF *af, BitSet* bs, FILE *f);
 
 // Caution: The below methods do not check the index k !
 // Set bit at index k
@@ -56,35 +57,38 @@ void print_bitset(BitSet* bs, FILE *f);
 // Caution: The methods below do not check and compare the sizes of bs1 and bs2
 
 // Return 1 if bs1 is subset of bs2, otherwise 0.
-char bitset_is_subset(BitSet* bs1, BitSet* bs2);
+char bitset_is_subset(AF *af, BitSet* bs1, BitSet* bs2);
 
-char bitset_is_equal(BitSet* bs1, BitSet* bs2);
+char bitset_is_equal(AF *af, BitSet* bs1, BitSet* bs2);
 
 // Intersect bs1 and bs2, store the result in r
-void bitset_intersection(BitSet* bs1, BitSet* bs2, BitSet* r);
+void bitset_intersection(AF *af, BitSet* bs1, BitSet* bs2, BitSet* r);
 
 // Unite bs1 and bs2, store the result in r
-void bitset_union(BitSet* bs1, BitSet* bs2, BitSet* r);
+void bitset_union(AF *af, BitSet* bs1, BitSet* bs2, BitSet* r);
 
 // Negate bitset (flip the bits) bs and store the result in r.
-void complement_bitset(BitSet* bs, BitSet* r);
+void complement_bitset(AF *af, BitSet* bs, BitSet* r);
 
 // Compute set difference bs1 \ bs2, store it in r.
-void bitset_set_minus(BitSet* bs1, BitSet* bs2, BitSet* r);
+void bitset_set_minus(AF *af, BitSet* bs1, BitSet* bs2, BitSet* r);
 
 // Return true if all bits are set, otherwise false
-char bitset_is_fullset(BitSet* bs);
+char bitset_is_fullset(AF *af, BitSet* bs);
 
 // Return true if all bits are 0, otherwise false
-char bitset_is_emptyset(BitSet* bs);
+char bitset_is_emptyset(AF *af, BitSet* bs);
 
 // Copy bs1 into bs2
-void copy_bitset(BitSet* bs1, BitSet* bs2);
+void copy_bitset(AF *af, BitSet* bs1, BitSet* bs2);
 
 // Clear all bits
-void reset_bitset(BitSet* bs);
+void reset_bitset(AF *af, BitSet* bs);
+
+// Set all bits
+void set_bitset(AF *af, BitSet* bs);
 
 // Return the number of bits set to 1
-int bitset_get_length(BitSet* bs);
+// int bitset_get_length(BitSet* bs);
 
 #endif /* BITSET_BITSET_H_ */
