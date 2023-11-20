16d5dc52ebd66f765bf177fd65f3438d5eb193b3
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2023-11-20 11:34:14 +0100

    Minor changes on implications. Nourine method uses implications for self-attacks.

--- src/algorithms/next-closure/stable.c
+++ src/algorithms/next-closure/stable.c
@@ -91,7 +91,7 @@ void stable_extensions_nc(AF *attacks, FILE *outfile) {
 
 		if (bitset_is_equal(c, c_up)) {
 			++stable_extension_count;
-			print_set(c, outfile);
+			print_set(c, outfile, "\n");
 		}
 		copy_bitset(c, tmp);
 	}
@@ -123,7 +123,7 @@ void one_stable_extension_nc(AF* attacks, FILE *outfile) {
 		up_arrow(not_attacks, c, c_up);
 
 		if (bitset_is_equal(c, c_up)) {
-			print_set(c, outfile);
+			print_set(c, outfile, "\n");
 			break;
 		}
 		copy_bitset(c, tmp);
