b9f8ecacb97c4fe6231a440acfa553feb8af13e5
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2024-02-16 15:36:24 +0100

    added hashing and some examples how to use it.

---
+++ src/hashing/dynamic_hash_table.c
@@ -0,0 +1,137 @@
+/*
+ * The ELepHant Reasoner
+ *
+ * Copyright (C) Baris Sertkaya (sertkaya@fb2.fra-uas.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "dynamic_hash_table.h"
+#include "utils.h"
+
+/**
+ * A dynamic hash table implementation for storing keys only, no associated values.
+ * The allocated space is doubled once the load factor reaches 0.75.
+ * Uses open addressing with linear probing.
+ * Keys cannot be NULL.
+ * Allocated space is not shrunk after a removal.
+ */
+
+
+void dynamic_hash_table_init(DynamicHashTable* hash_table, unsigned int size) {
+
+	if (size < 8)
+		size = 8;
+	else
+		size = roundup_pow2(size);
+
+	hash_table->elements = (void**) calloc(size, sizeof(void*));
+	assert(hash_table->elements != NULL);
+	hash_table->end_indexes = (unsigned int*) calloc(size, sizeof(unsigned int));
+	assert(hash_table->end_indexes != NULL);
+	hash_table->size = size;
+	hash_table->element_count = 0;
+	int i;
+	for (i = 0; i < hash_table->size; ++i)
+		// each chain initially ends at its starting point
+		hash_table->end_indexes[i] = i;
+
+	return;
+}
+
+DynamicHashTable* dynamic_hash_table_create(unsigned int size) {
+	if (size < 8)
+		size = 8;
+	else
+		size = roundup_pow2(size);
+	DynamicHashTable* hash_table = (DynamicHashTable*) malloc(sizeof(DynamicHashTable));
+	assert(hash_table != NULL);
+
+	hash_table->elements = (void**) calloc(size, sizeof(void*));
+	assert(hash_table->elements != NULL);
+	hash_table->end_indexes = (unsigned int*) calloc(size, sizeof(unsigned int));
+	assert(hash_table->end_indexes != NULL);
+	hash_table->size = size;
+	hash_table->element_count = 0;
+	int i;
+	for (i = 0; i < hash_table->size; ++i)
+		// each chain initially ends at its starting point
+		hash_table->end_indexes[i] = i;
+
+	return hash_table;
+}
+
+extern inline char dynamic_hash_table_insert(void* key, DynamicHashTable* hash_table);
+
+extern inline char dynamic_hash_table_contains(void* key, DynamicHashTable* hash_table);
+
+extern char dynamic_hash_table_remove(void* key, DynamicHashTable* hash_table);
+
+/**
+ * Create an iterator for the given hash table.
+ */
+extern inline DynamicHashTableIterator* dynamic_hash_table_iterator_create(DynamicHashTable* h);
+
+extern inline void dynamic_hash_table_iterator_init(DynamicHashTableIterator* iterator, DynamicHashTable* hash_table);
+
+/**
+ * Get the next element.
+ * Returns NULL if there is no next element.
+ */
+extern inline void* dynamic_hash_table_iterator_next(DynamicHashTableIterator* iterator);
+
+
+
+int dynamic_hash_table_free(DynamicHashTable* hash_table) {
+	int i;
+	int freed_bytes = 0;
+
+	free(hash_table->elements);
+	freed_bytes += hash_table->size * sizeof(void*);
+
+	free(hash_table->end_indexes);
+	freed_bytes += hash_table->size * sizeof(unsigned int);
+
+	free(hash_table);
+	freed_bytes += sizeof(DynamicHashTable);
+
+	return freed_bytes;
+}
+
+int dynamic_hash_table_reset(DynamicHashTable* hash_table) {
+	int i;
+	int freed_bytes = 0;
+
+	free(hash_table->elements);
+	freed_bytes += hash_table->size * sizeof(void*);
+
+	free(hash_table->end_indexes);
+	freed_bytes += hash_table->size * sizeof(unsigned int);
+
+	hash_table->element_count = 0;
+	hash_table->size = 0;
+
+	return freed_bytes;
+}
+
+inline int dynamic_hash_table_iterator_free(DynamicHashTableIterator* iterator) {
+	free(iterator);
+
+	return sizeof(DynamicHashTableIterator);
+}
---
+++ src/test/test_bitset_map.c
@@ -0,0 +1,96 @@
+/*
+ * The ELepHant Reasoner
+ *
+ * Copyright (C) Baris Sertkaya (sertkaya@fb2.fra-uas.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/time.h>
+
+#include "../bitset/bitset.h"
+#include "../utils/map.h"
+#include "../utils/set.h"
+
+#define MAP_INITIAL_SIZE 	100
+#define SET_INITIAL_SIZE 	100
+#define BITSET_SIZE			20
+
+int main(int argc, char *argv[]) {
+
+	// create a bit set (a subset of the argument set)
+	// it will be the key of the map
+	BitSet*  x = create_bitset(BITSET_SIZE);
+	// add some arguments to it
+	SET_BIT(x, 10);
+	SET_BIT(x, 15);
+	SET_BIT(x, 80);
+	SET_BIT(x, 90);
+	SET_BIT(x, 197);
+	SET_BIT(x, 198);
+	SET_BIT(x, 199);
+
+	// create a set
+	Set* s = SET_CREATE(SET_INITIAL_SIZE);
+	// create 10 bitsets (subsets of the argument set)
+	int i,j;
+	for (i = 0; i < 10; ++i) {
+		BitSet*  y = create_bitset(BITSET_SIZE);
+		// each bitset has 2 arguments
+		for (j = 2 * i; j < 2 * i + 2; ++j) {
+			SET_BIT(y, j);
+		}
+		// add the bitset y to the set s
+		SET_ADD(y, s);
+	}
+
+	// now key is the bitset "x", value is the set of bitsets "s"
+	// put the key value pair (x,s) to the map
+
+
+	// define the map
+	Map map;
+	// initialize it
+	MAP_INIT(&map, MAP_INITIAL_SIZE);
+	// put (x,s) to the map
+	MAP_PUT((uint64_t) x, s, &map);
+
+	// iterate over the map
+	MapIterator map_it;
+	MAP_ITERATOR_INIT(&map_it, &map);
+	Set* e = MAP_ITERATOR_NEXT(&map_it);
+	printf("Iterating .........................:\n");
+	int count = 0;
+	while (e) {
+		printf("value %d:\n", count++);
+		// e is the value, in our case the set of bitsets
+		// iterate over it and print
+		SetIterator* set_it = SET_ITERATOR_CREATE((Set*) e);
+		BitSet* bs = SET_ITERATOR_NEXT(set_it);
+		while (bs != NULL) {
+			print_bitset(bs, stdout);
+			printf("\n");
+			bs = SET_ITERATOR_NEXT(set_it);
+		}
+		SET_ITERATOR_FREE(set_it);
+		e = MAP_ITERATOR_NEXT(&map_it);
+	}
+
+
+	return 1;
+}
+
+
