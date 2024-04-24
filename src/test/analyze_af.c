3f50571acb166b70d5d9ffc14337b45023816cfc
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2024-04-24 12:54:56 +0200

    Small test code for printing framework statistics

---
+++ src/test/analyze_af.c
@@ -0,0 +1,37 @@
+/*
+ * AFCA - argumentation framework using closed sets
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "../af/af.h"
+#include "../parser/af_parser.h"
+#include "../algorithms/nourine/stable.h"
+#include "../algorithms/nourine/implications.h"
+
+int main(int argc, char *argv[]) {
+
+	FILE *fd = fopen(argv[1], "r");
+	assert(fd != NULL);
+
+	// just read the af and print the statistics
+	AF* af = read_af(fd);
+
+	return(0);
+}
+
+
