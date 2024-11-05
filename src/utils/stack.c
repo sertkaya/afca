0a88a0a247a1962c99ccef5898300b0438c01425
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2024-11-05 16:10:20 +0100

    Added stack.

---
+++ src/utils/stack.c
@@ -0,0 +1,29 @@
+/*
+ * AFCA - argumentation framework using closed sets
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+
+#include "stack.h"
+
+extern inline void push(Stack*s, void* e);
+extern inline void* pop(Stack* s);
+
+void init_stack(Stack* s) {
+	s->size = 0;
+	s->elements = NULL;
+}
+
