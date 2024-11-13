3e0f587b58f9301a3ce52238f5a5041191575a2b
sergei.obj <s-obj@yandex.ru>
2024-11-13 11:07:24 +0100

    SE-ST with max independent sets

--- src/algorithms/maximal-independent-sets/mis.h
+++ src/algorithms/maximal-independent-sets/mis.h
@@ -2,8 +2,11 @@
 #define AF_MAXIMAL_INDEPENDENT_SETS_H_
 
 #include "../../af/af.h"
+#include "../../bitset/bitset.h"
 #include "../../utils/linked_list.h"
 
 ListNode*  ee_st_maximal_independent_sets(AF* af);
+
+BitSet* se_st_mis(AF* af);
 
 #endif /* AF_MAXIMAL_INDEPENDENT_SETS_H_ */
