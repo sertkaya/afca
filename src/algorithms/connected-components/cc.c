6c3d25a3fd4a243c06028ef2ef2c3192c3093cc3
sergei.obj <s-obj@yandex.ru>
2025-01-10 17:04:17 +0100

    scc-next-closure for EE-PR

--- src/algorithms/connected-components/cc.c
+++ src/algorithms/connected-components/cc.c
@@ -18,8 +18,10 @@
 
 #include "../../bitset/bitset.h"
 #include "../maximal-independent-sets/mis.h"
+#include "../next-closure/preferred.h"
 #include "../norris/stable.h"
 #include "../nourine/stable.h"
+#include "cc.h"
 #include "scc.h"
 #include "wcc.h"
 
@@ -52,9 +54,11 @@ void run_cc(AF* af, ListNode* (*stable_extensions)(AF* af), FILE* output, bool s
 	free_argumentation_framework(af);
 }
 
+
 void run_cc_mis(AF* af, FILE* output, bool scc) {
 	run_cc(af, ee_st_maximal_independent_sets, output, scc);
 }
+
 
 void run_cc_norris(AF* af, FILE* output, bool scc) {
 	run_cc(af, ee_st_norris, output, scc);
@@ -70,3 +74,27 @@ void run_cc_nourine(AF* af, FILE* output, bool scc) {
 	run_cc(af, enumerate_stable_extensions_via_implications, output, scc);
 }
 
+
+void run_scc_next_closure(AF* af, FILE* output) {
+	run_cc_preferred(af, ee_pr_next_closure, output);
+}
+
+
+void run_cc_preferred(AF* af, ListNode* (*preferred_extensions)(AF* af), FILE* output) {
+	ListNode* head = ee_pr_scc(af, preferred_extensions);
+	ListNode* node = head;
+
+	while (node) {
+		print_extension(node->c, output);
+		node = node->next;
+	}
+
+	while (head) {
+		ListNode* next = head->next;
+		free_bitset((BitSet*) head->c);
+		free(head);
+		head = next;
+	}
+
+	// af is freed by ee_pr_scc
+}
