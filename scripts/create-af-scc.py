e1d65096f029e2510bef515363c54297aed1f488
sergei.obj <s-obj@yandex.ru>
2026-03-04 17:18:48 +0100

    added outer probability to scc generator

--- scripts/create-af-scc.py
+++ scripts/create-af-scc.py
@@ -10,7 +10,8 @@ parser = argparse.ArgumentParser()
 
 # Adding optional parameter
 parser.add_argument('args_count', help="Number of arguments")
-parser.add_argument('density', help="Density of the attacks relation")
+parser.add_argument('inner_probability', help="Probability of the attacks relation")
+parser.add_argument('outer_probability', help="Probability of the attacks relation")
 parser.add_argument('scc_count', help="Minimum number of strongly connected components")
 parser.add_argument('out_file', help = "Output file")
 
@@ -20,28 +21,33 @@ params = parser.parse_args()
 out_file_af = open(str(params.out_file) + ".af", "w")
 out_file_apx = open(str(params.out_file) + ".apx", "w")
 arg_count = int(params.args_count)
-density = float(params.density)
+inner_probability = float(params.inner_probability)
+outer_probability = float(params.outer_probability)
 scc_count = int(params.scc_count)
 
-if (density > 1):
-    sys.exit("Density cannot be larger than 1.")
+if (inner_probability > 1 or outer_probability > 1):
+    sys.exit("Probability cannot be larger than 1.")
 
 component = []
 while set(component) != set(range(scc_count)):
     component = [random.randrange(scc_count) for i in range(arg_count)]
     
+outer_attacks = [[random.random() < 0.3 for d in range(scc_count)]
+                 for c in range(scc_count)]
 # dictionary of attacks
 attacks = collections.defaultdict(set)
 for i in range(arg_count):
     for j in range(arg_count):
-        # if i == j:
-        #     continue
-        if random.random() < density:
-            if component[i] <= component[j]:
+        if i == j:
+            continue
+        if component[i] == component[j]:
+            if random.random() < inner_probability:
                 attacks[i + 1].add(j + 1)
-            else:
-                attacks[j + 1].add(i + 1)
-                
+        elif (component[i] < component[j] and
+              outer_attacks[component[i]][component[j]] and
+              random.random() < outer_probability):
+            attacks[i + 1].add(j + 1)
+
 
 out_file_af.write("p af " + str(arg_count) + "\n")
 for arg in attacks.keys():
